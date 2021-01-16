79b659487c91acb1445349c1f0fe820e9d7c81f9
unknown <ygorsallesaniceto@gmail.com>
2021-01-16 18:03:30 -0300

    backref - delete cascade

--- Python/ORM-SQLAlchemy-relacionamentos/rel2-1-1-deleteCascade.py
+++ Python/ORM-SQLAlchemy-relacionamentos/rel2-1-1-deleteCascade.py
@@ -2,7 +2,7 @@ from sqlalchemy import (create_engine, Column, Integer, String, ForeignKey)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import backref, sessionmaker, relationship
 
-engine = create_engine('postgresql+psycopg2://postgres:123456@localhost:5432/teste', echo=True)
+engine = create_engine('postgresql+psycopg2://postgres:123456@localhost:5432/teste', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
@@ -12,7 +12,7 @@ class Department(Base):
     __tablename__ = 'department'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    employee = relationship('Employee', uselist=False, back_populates='department', cascade="all, delete", passive_deletes=True)
+    employee = relationship('Employee', uselist=False, backref='department', cascade="all, delete", passive_deletes=True)
 
     def __repr__(self):
         return f'Department=(id={self.id}, name={self.name}, employee={self.employee})'
@@ -22,14 +22,13 @@ class Employee(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     departament_id = Column(Integer, ForeignKey('department.id', ondelete='CASCADE'), unique=True)
-    department = relationship('Department', back_populates='employee')
 
     def __repr__(self):
         return f'Employee=(id={self.id}, name={self.name}, departament_id={self.departament_id})'
 
 # Um departamento possui um funcionário, e um funcionário só pode estar em um departamento. Se o
 # departamento for excluído todos empregados associados a ele serão excluídos também
-# pode-se associar com back-populates ou backref. No caso do backref deve se colocar em apenas uma tabela
+# pode-se associar com back-populates ou backref. No caso do back_populates em duas tabelas
 
 Base.metadata.create_all(engine)
 
@@ -51,16 +50,17 @@ Base.metadata.create_all(engine)
 # session.commit()
 # session.close()
 
-# array = session.query(Employee).all()
-# for i in array:
-#     print('\n', i.id, i.name, i.department.id, i.department.name)
+# it_department = session.query(Department).filter(Department.id == 1).first()
+# session.delete(it_department)
 # session.commit()
 # session.close()
 
-it_department = session.query(Department).filter(Department.id == 1).first()
-session.delete(it_department)
-session.commit()
-session.close()
-
 # from pprint import pprint
 # pprint(session.query(Department).all())
+# session.commit()
+# session.close()
+
+from pprint import pprint
+pprint(session.query(Employee).all())
+session.commit()
+session.close()
