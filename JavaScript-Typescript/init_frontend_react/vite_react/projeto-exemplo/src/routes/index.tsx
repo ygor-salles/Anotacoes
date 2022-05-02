59985b054104b8809312822bc9299cef922ef34e
ygor-salles <ygorsallesaniceto@gmail.com>
2022-05-02 20:58:29 -0300

    feat: adding vite-react sample project with react-router-dom and folder structure

---
+++ JavaScript-Typescript/init_frontend_react/vite_react/projeto-exemplo/src/routes/index.tsx
@@ -0,0 +1,21 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes as Switch,
+  Navigate,
+} from 'react-router-dom';
+
+import { Dashboard, Login } from '../pages';
+
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/entrar" element={<Login />} />
+        <Route path="/pagina-inicial" element={<Dashboard />} />
+
+        <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
